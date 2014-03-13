9623fcf37f5a9e7f808f944da6b26b59c4b01aa1
TJ Holowaychuk <tj@vision-media.ca>
2014-03-12 19:13:01 -0700

    add newlines to the tests

--- test.js
+++ test.js
@@ -9,8 +9,10 @@ describe('Koa Session', function(){
     describe('when app.keys are set', function(){
       it('should work', function(done){
         var app = koa();
+
         app.keys = ['a', 'b'];
         app.use(session());
+
         app.use(function *(){
           this.session.message = 'hi';
           this.body = this.session;
@@ -25,7 +27,9 @@ describe('Koa Session', function(){
     describe('when app.keys are not set', function(){
       it('should throw', function(done){
         var app = koa();
+
         app.use(session());
+
         app.use(function *(){
           this.session.message = 'hi';
           this.body = this.session;
@@ -42,9 +46,9 @@ describe('Koa Session', function(){
     describe('when app.keys are not set', function(){
       it('should work', function(done){
         var app = koa();
-        app.use(session({
-          signed: false
-        }));
+
+        app.use(session({ signed: false }));
+
         app.use(function *(){
           this.session.message = 'hi';
           this.body = this.session;
@@ -60,6 +64,7 @@ describe('Koa Session', function(){
   describe('when the session contains a ;', function(){
     it('should still work', function(done){
       var app = App();
+
       app.use(function *(){
         if (this.method === 'POST') {
           this.session.string = ';';
@@ -68,7 +73,9 @@ describe('Koa Session', function(){
           this.body = this.session.string;
         }
       });
+
       var server = app.listen();
+
       request(server)
       .post('/')
       .expect(204, function(err, res){
@@ -86,9 +93,11 @@ describe('Koa Session', function(){
     describe('when not accessed', function(){
       it('should not Set-Cookie', function(done) {
         var app = App();
+
         app.use(function *(){
           this.body = 'greetings';
         })
+
         request(app.listen())
         .get('/')
         .expect(200, function(err, res){
@@ -102,10 +111,12 @@ describe('Koa Session', function(){
     describe('when accessed and not populated', function(done){
       it('should not Set-Cookie', function(done) {
         var app = App();
+
         app.use(function *(){
           this.session;
           this.body = 'greetings';
         })
+
         request(app.listen())
         .get('/')
         .expect(200, function(err, res){
@@ -119,10 +130,12 @@ describe('Koa Session', function(){
     describe('when populated', function(done){
       it('should Set-Cookie', function(done){
         var app = App();
+
         app.use(function *(){
           this.session.message = 'hello';
           this.body = '';
         })
+
         request(app.listen())
         .get('/')
         .expect('Set-Cookie', /koa:sess/)
@@ -135,9 +148,11 @@ describe('Koa Session', function(){
 
       it('should not Set-Cookie', function(done){
         var app = App();
+
         app.use(function *(){
           this.body = this.session;
         })
+
         request(app.listen())
         .get('/')
         .expect(200, function(err, res){
@@ -153,9 +168,11 @@ describe('Koa Session', function(){
     describe('when not accessed', function(){
       it('should not Set-Cookie', function(done){
         var app = App();
+
         app.use(function *(){
           this.body = 'aklsdjflasdjf';
         })
+
         request(app.listen())
         .get('/')
         .set('Cookie', cookie)
@@ -170,10 +187,12 @@ describe('Koa Session', function(){
     describe('when accessed but not changed', function(){
       it('should be the same session', function(done){
         var app = App();
+
         app.use(function *(){
           this.session.message.should.equal('hello');
           this.body = 'aklsdjflasdjf';
         })
+
         request(app.listen())
         .get('/')
         .set('Cookie', cookie)
@@ -182,10 +201,12 @@ describe('Koa Session', function(){
 
       it('should not Set-Cookie', function(done){
         var app = App();
+
         app.use(function *(){
           this.session.message.should.equal('hello');
           this.body = 'aklsdjflasdjf';
         })
+
         request(app.listen())
         .get('/')
         .set('Cookie', cookie)
@@ -200,10 +221,12 @@ describe('Koa Session', function(){
     describe('when accessed and changed', function(){
       it('should Set-Cookie', function(done){
         var app = App();
+
         app.use(function *(){
           this.session.money = '$$$';
           this.body = 'aklsdjflasdjf';
         })
+
         request(app.listen())
         .get('/')
         .set('Cookie', cookie)
@@ -213,14 +236,16 @@ describe('Koa Session', function(){
     })
   })
 
-  describe('when session = ', function(){
+  describe('when session is', function(){
     describe('null', function(){
       it('should expire the session', function(done){
         var app = App();
+
         app.use(function *(){
           this.session = null;
           this.body = 'asdf';
         })
+
         request(app.listen())
         .get('/')
         .expect('Set-Cookie', /koa:sess/)
@@ -231,10 +256,12 @@ describe('Koa Session', function(){
     describe('{}', function(){
       it('should not Set-Cookie', function(done){
         var app = App();
+
         app.use(function *(){
           this.session = {};
           this.body = 'asdf';
         })
+
         request(app.listen())
         .get('/')
         .expect(200, function(err, res){
@@ -248,10 +275,12 @@ describe('Koa Session', function(){
     describe('{a: b}', function(){
       it('should create a session', function(done){
         var app = App();
+
         app.use(function *(){
           this.session = { message: 'hello' };
           this.body = 'asdf';
         })
+
         request(app.listen())
         .get('/')
         .expect('Set-Cookie', /koa:sess/)
@@ -262,9 +291,11 @@ describe('Koa Session', function(){
     describe('anything else', function(){
       it('should throw', function(done){
         var app = App();
+
         app.use(function *(){
           this.session = 'asdf'
         })
+
         request(app.listen())
         .get('/')
         .expect(500, done);
@@ -275,7 +306,9 @@ describe('Koa Session', function(){
   describe('when an error is thrown downstream and caught upstream', function(){
     it('should still save the session', function(done){
       var app = koa();
+
       app.keys = ['a', 'b'];
+
       app.use(function *(next){
         try {
           yield *next;
@@ -284,14 +317,18 @@ describe('Koa Session', function(){
           this.body = err.message;
         }
       });
+
       app.use(session());
+
       app.use(function *(next){
         this.session.name = 'funny';
         yield *next;
       });
+
       app.use(function *(next){
         this.throw(401);
       });
+
       request(app.listen())
       .get('/')
       .expect('Set-Cookie', /koa:sess/)

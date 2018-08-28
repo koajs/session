085b4784d83bb61c3ebb53952109337fe6e9f48f
Jonas Galvez <jonasgalvez@gmail.com>
2018-08-28 01:25:09 -0300

    Added autoCommit: false test

--- test/contextstore.test.js
+++ test/contextstore.test.js
@@ -356,6 +356,32 @@ describe('Koa Session External Context Store', () => {
     });
   });
 
+  describe('when autoCommit is present', () => {
+    describe('and set to false', () => {
+      it('should not set headers if commit() isn\'t called', done => {
+        const app = App({ autoCommit: false });
+        app.use(async function(ctx) {
+          if (ctx.method === 'POST') {
+            ctx.session.message = 'hi';
+            ctx.body = 200;
+            return;
+          }
+          ctx.body = ctx.session.message;
+        });
+        const server = app.listen();
+
+        request(server)
+        .post('/')
+        .end((err, res) => {
+          if (err) return done(err);
+          const cookie = res.headers['set-cookie'];
+          should.not.exists(cookie);
+        })
+        .expect(200, done);
+      });
+    });
+  });
+
   describe('when maxAge present', () => {
     describe('and set to be a session cookie', () => {
       it('should not expire the session', done => {
